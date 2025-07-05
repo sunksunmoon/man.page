bfa2024ddb1c83b259495d2be9d5b08755684998
man <2400018185@webmail.uad.ac.id>
2025-07-05 07:02:52 +0700

    Update tugas ktp.cpp

--- tugas ktp.cpp
+++ tugas ktp.cpp
@@ -25,12 +25,7 @@ private:
 
     void pilih_agama() {
         cout << "Pilih Agama anda:\n";
-        cout << "1. Islam\n";
-        cout << "2. Kristen\n";
-        cout << "3. Katolik\n";
-        cout << "4. Hindu\n";
-        cout << "5. Buddha\n";
-        cout << "6. Konghucu\n";
+        cout << "1. Islam\n2. Kristen\n3. Katolik\n4. Hindu\n5. Buddha\n6. Konghucu\n";
         cout << "Pilihan (1-6): ";
         cin >> pil_agama;
 
@@ -43,7 +38,7 @@ private:
             case 6: agama = "Konghucu"; break;
             default:
                 cout << "Pilihan tidak valid. Ulangi.\n";
-                pilih_agama(); // Rekursi
+                pilih_agama();
         }
     }
 
@@ -51,85 +46,68 @@ public:
     void input() {
         cin.ignore();
         cout << "\nInput Data KTP\n=========================\n";
-        cout << "Nama                : ";
-        getline(cin, nama);
-        cout << "NIK                 : ";
-        cin >> nik;
-        cin.ignore();
-        cout << "Tempat Lahir        : ";
-        getline(cin, tempat_l);
-        cout << "Tanggal Lahir       : ";
-        cin >> tanggal_l;
-        cout << "Bulan Lahir         : ";
-        cin >> bulan_l;
-        cout << "Tahun Lahir         : ";
-        cin >> tahun_l;
-        pilih_jk();
-        cin.ignore();
-        cout << "Golongan Darah      : ";
-        getline(cin, gol);
-        cout << "Alamat              : ";
-        getline(cin, alamat);
-        cout << "RT/RW               : ";
-        getline(cin, rt);
-        cout << "Kel/Desa            : ";
-        getline(cin, desa);
-        cout << "Kecamatan           : ";
-        getline(cin, kecamatan);
-        pilih_agama();
-        cin.ignore();
-        cout << "Status Perkawinan   : ";
-        getline(cin, status_p);
-        cout << "Pekerjaan           : ";
-        getline(cin, pekerjaan);
-        cout << "Kewarganegaraan     : ";
-        getline(cin, kewarganegaraan);
-        cout << "Berlaku Hingga      : ";
-        getline(cin, bh);
+        cout << "Nama                : "; getline(cin, nama);
+        cout << "NIK                 : "; cin >> nik; cin.ignore();
+        cout << "Tempat Lahir        : "; getline(cin, tempat_l);
+        cout << "Tanggal Lahir       : "; cin >> tanggal_l;
+        cout << "Bulan Lahir         : "; cin >> bulan_l;
+        cout << "Tahun Lahir         : "; cin >> tahun_l;
+        pilih_jk(); cin.ignore();
+        cout << "Golongan Darah      : "; getline(cin, gol);
+        cout << "Alamat              : "; getline(cin, alamat);
+        cout << "RT/RW               : "; getline(cin, rt);
+        cout << "Kel/Desa            : "; getline(cin, desa);
+        cout << "Kecamatan           : "; getline(cin, kecamatan);
+        pilih_agama(); cin.ignore();
+        cout << "Status Perkawinan   : "; getline(cin, status_p);
+        cout << "Pekerjaan           : "; getline(cin, pekerjaan);
+        cout << "Kewarganegaraan     : "; getline(cin, kewarganegaraan);
+        cout << "Berlaku Hingga      : "; getline(cin, bh);
     }
 
     void tampil() {
-    	cout << "\n+=========================================================+\n";
-    	cout << "|            KARTU TANDA PENDUDUK (KTP) ELEKTRONIK        |\n";
-    	cout << "+=========================================================+\n";
-    	cout << "| NIK                 : " << nik << endl;
-    	cout << "| Nama                : " << nama << endl;
-    	cout << "| Tempat/Tgl Lahir    : " << tempat_l << ", " << tanggal_l << "-" << bulan_l << "-" << tahun_l << endl;
-    	cout << "| Jenis Kelamin       : " << gender;
-    	cout << "     Gol. Darah : " << gol << endl;
-    	cout << "| Alamat              : " << alamat << endl;
-    	cout << "|    RT/RW            : " << rt << endl;
-    	cout << "|    Kel/Desa         : " << desa << endl;
-    	cout << "|    Kecamatan        : " << kecamatan << endl;
-    	cout << "| Agama               : " << agama << endl;
-    	cout << "| Status Perkawinan   : " << status_p << endl;
-    	cout << "| Pekerjaan           : " << pekerjaan << endl;
-    	cout << "| Kewarganegaraan     : " << kewarganegaraan << endl;
-    	cout << "| Berlaku Hingga      : " << bh << endl;
-    	cout << "+=========================================================+\n";
-}
-
+        cout << "\n+=========================================================+\n";
+        cout << "|            KARTU TANDA PENDUDUK (KTP) ELEKTRONIK        |\n";
+        cout << "+=========================================================+\n";
+        cout << "| NIK                 : " << nik << endl;
+        cout << "| Nama                : " << nama << endl;
+        cout << "| Tempat/Tgl Lahir    : " << tempat_l << ", " << tanggal_l << "-" << bulan_l << "-" << tahun_l << endl;
+        cout << "| Jenis Kelamin       : " << gender << "     Gol. Darah : " << gol << endl;
+        cout << "| Alamat              : " << alamat << endl;
+        cout << "|    RT/RW            : " << rt << endl;
+        cout << "|    Kel/Desa         : " << desa << endl;
+        cout << "|    Kecamatan        : " << kecamatan << endl;
+        cout << "| Agama               : " << agama << endl;
+        cout << "| Status Perkawinan   : " << status_p << endl;
+        cout << "| Pekerjaan           : " << pekerjaan << endl;
+        cout << "| Kewarganegaraan     : " << kewarganegaraan << endl;
+        cout << "| Berlaku Hingga      : " << bh << endl;
+        cout << "+=========================================================+\n";
+    }
 
     string get_nama() const { return nama; }
 };
 
-void sort_ktp(KTP arr[], int n) {
+// Sorting dengan pointer
+void sort_ktp(KTP* arr, int n) {
     for (int i = 0; i < n - 1; i++) {
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j].get_nama() > arr[j + 1].get_nama()) {
-                KTP temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+            if ((arr + j)->get_nama() > (arr + j + 1)->get_nama()) {
+                KTP temp = *(arr + j);
+                *(arr + j) = *(arr + j + 1);
+                *(arr + j + 1) = temp;
             }
         }
     }
 }
 
-int cari_nama_rekursif(KTP arr[], int kiri, int kanan, string target) {
+// Pencarian rekursif dengan pointer
+int cari_nama_rekursif(KTP* arr, int kiri, int kanan, string target) {
     if (kiri > kanan) return -1;
     int tengah = (kiri + kanan) / 2;
-    if (arr[tengah].get_nama() == target) return tengah;
-    else if (arr[tengah].get_nama() > target)
+    if ((arr + tengah)->get_nama() == target)
+        return tengah;
+    else if ((arr + tengah)->get_nama() > target)
         return cari_nama_rekursif(arr, kiri, tengah - 1, target);
     else
         return cari_nama_rekursif(arr, tengah + 1, kanan, target);
@@ -140,11 +118,12 @@ int main() {
     cout << "Masukkan jumlah data KTP yang ingin diinput: ";
     cin >> jumlah;
 
-    KTP *data = new KTP[jumlah];
+    // Alokasi dinamis pointer ke array KTP
+    KTP* data = new KTP[jumlah];
 
     for (int i = 0; i < jumlah; i++) {
         cout << "\nData ke-" << (i + 1) << endl;
-        data[i].input();
+        (data + i)->input();  // akses via pointer
     }
 
     sort_ktp(data, jumlah);
@@ -153,7 +132,7 @@ int main() {
     cout << "\nData setelah diurutkan berdasarkan nama:\n";
     for (int i = 0; i < jumlah; i++) {
         cout << "\nData ke-" << (i + 1) << endl;
-        data[i].tampil();
+        (data + i)->tampil();  // akses via pointer
     }
 
     cin.ignore();
@@ -164,7 +143,7 @@ int main() {
     int posisi = cari_nama_rekursif(data, 0, jumlah - 1, dicari);
     if (posisi != -1) {
         cout << "\nData ditemukan pada indeks ke-" << posisi << ":\n";
-        data[posisi].tampil();
+        (data + posisi)->tampil();  // akses via pointer
     } else {
         cout << "Data tidak ditemukan.\n";
     }
@@ -172,4 +151,3 @@ int main() {
     delete[] data;
     return 0;
 }
-
